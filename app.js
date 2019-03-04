325cf672eb8ce1eb774229efd20dafd00c781561
George Jacob <georgekunchattil@gmail.com>
2019-03-04 19:46:11 +0530

    Convert text to uppercase

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular.module('DIApp', [])
+    .controller('DIController', ['$scope', '$filter', DIController]);
+
+    function DIController ($scope, $filter) {  // filter is a service that let us create filtering functions that are used for formatting the data that eventually gets displayed to the user
+        $scope.name ="Yaakov";
+
+        $scope.upper = function () {
+            var upCase =$filter('uppercase')
+            $scope.name = upCase($scope.name);
+        };
+    }
+
+})();
